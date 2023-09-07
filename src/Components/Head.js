1465bd9f4dfc7e04af4a3b22bf7e2565f7327a36
dmark140 <famolerasd@gmail.com>
2023-09-07 18:46:48 +0800

    09072023B

---
+++ src/Components/Head.js
@@ -0,0 +1,20 @@
+import React from "react";
+export default function Head() {
+  return (
+    <div className="flex hghasdx grid-cols-2 ">
+      <div>
+        <img
+          src="https://avatars.githubusercontent.com/u/14121234?s=400&u=c4ff30b44d17298cd8a0fded6258b505007fa891&"
+          className="profile"
+        />
+      </div>
+      <div>
+        <p className="text-Regular p-0 -m-0 text-xl">Deanmark Famoleras</p>
+        <p className="text-PreMute p-0 -m-0  text-sm">
+          Innovation focused Systems Admin
+        </p>
+        <a className="text-Mute p-0 -m-0  text-sm">@CodeWithDean</a>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Head from "./Head";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <Head />
+    </div>
+  );
+}
